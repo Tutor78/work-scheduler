07c95d22b1b8e8632055c4a3fafa5892f214c8c6
Tutor <tutor.michael78@gmail.com>
2021-09-20 20:00:21 -0500

    added employee/job tables

---
+++ models/Job.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Job extends Model {}
+
+// define the jobs table
+Job.init(
+    {
+        // sets the id column info
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        // pass in the sequelize connection
+        sequelize,
+        // disable automatically adding createAt/updatedAt fields
+        timestamps: false,
+        // does not allow the table to be pluralized
+        freezeTableName: true,
+        // use underscores instead of camelCasing
+        underscored: true,
+        // ensure the table name stays lowercase
+        modelName: 'job'
+    }
+);
+
+module.exports = Job;
