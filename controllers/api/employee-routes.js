1144ec2fc0e93c2df64ffa84cd18384b20f770ee
Tutor <tutor.michael78@gmail.com>
2021-09-22 19:06:16 -0500

    finished employee routes, added session cookies for login and logout

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { userInfo } = require('os');
 const { Employee, Job, Shift, Day } = require('../../models');
 
 // GET /api/employees/
@@ -123,27 +124,118 @@ router.post('/', (req, res) => {
             "shift_id": 1
         }
     */
-    
+    Employee.create({
+        id: req.body.id,
+        email: req.body.email,
+        password: req.body.password,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        job_id: req.body.job_id,
+        shift_id: req.body.shift_id
+    })
+    .then(dbEmployeeData => {
+        req.session.save(() => {
+            req.session.user_id = dbEmployeeData.id;
+            req.session.email = dbEmployeeData.email;
+            req.session.loggedIn = true;
+
+            res.json(dbEmployeeData);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 // POST /api/employees/login
 router.post('/login', (req, res) => {
     // logic to login a user
+    Employee.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbEmployeeData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbEmployeeData.id;
+            req.session.email = dbEmployeeData.email;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbEmployeeData, message: 'You are now logged in!' });            
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 // POST /api/employess/logout
 router.post('/logout', (req, res) => {
     // logic to log a user out
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/employees/:id
 router.put('/:id', (req, res) => {
     // logic to update an employee
+    Employee.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'There is no user with that id! '});
+            return;
+        }
+
+        res.json(dbEmployeeData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 // DELETE /api/employees/:id
 router.delete('/:id', (req, res) => {
     // logic to delete an employee
+    Employee.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'There is no employee with that id!' });
+            return;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const session = require('express-session');
 
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: process.env.SECRET,
+    cookie:{},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
