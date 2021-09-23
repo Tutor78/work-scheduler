20e43993de11bf0aa11221024a88d7892226532b
Tutor <tutor.michael78@gmail.com>
2021-09-22 19:56:35 -0500

    added ability to search by department routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const employeeRoutes = require('./employee-routes');
 const jobRoutes = require('./job-routes');
+const departmentRoutes = require('./department-routes');
 
 router.use('/employees', employeeRoutes);
 router.use('/jobs', jobRoutes);
+router.use('/departments', departmentRoutes);
 
 module.exports = router;
