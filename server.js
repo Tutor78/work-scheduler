8a761a08de2c757ea90666ee0040fd7ee6ebc9c6
Michael Tutor <tutor.michael78@gmail.com>
2021-09-30 07:09:56 -0500

    added sign up modal

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'aIkdlOwpa122%iek',
+    secret: process.env.SECRET,
     cookie:{},
     resave: false,
     saveUninitialized: true,
